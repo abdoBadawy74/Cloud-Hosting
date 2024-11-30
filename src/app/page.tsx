7fd4b70afe119a2fe928c149457c400d7a802b91
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-11-30 18:02:28 +0200

    add Link component to routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
-
+import Link from "next/link"
 
 function HomePage() {
   return (
-    <div>Home Page</div>
+    <>
+      <div>Home Page</div>
+      <p> Welcome to Next js </p>
+      <div className="flex gap-2 text-blue-800">
+      <Link href="/about" > Go To About Page </Link>
+      <Link href="/admin" > Go To admin Page </Link>
+      <Link href="/register" > Go To register Page </Link>
+      <Link href="/login" > Go To login Page </Link>
+      </div>
+    </>
   )
 }
-
 export default HomePage
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function RegisterPage() {
-  return (
-    <div>
-        اهلا وسهلا
-    </div>
-  )
+  return <div>Register Page</div>;
 }
