89345fe46802ab4c0fb36df813e0d77e57c2bc64
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-05 00:52:44 +0200

    custom error page to single route

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function AdminPage() {
+  
+  throw new Error("Failed to fetch articles");
   return (
     <div>AdminPage</div>
   )
---
+++ src/app/articles/error.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Link from "next/link";
+interface ErrorPageProps {
+  error: Error;
+  reset: () => void;
+}
+
+function AriclesErrorPage({ error, reset }: ErrorPageProps) {
+  return (
+    <div className="pt-7 text-center ">
+        <p className="mb-3 text-xl">THis is custom error page for articles page/route </p>
+      <div className="text-xl text-red-600 font-semibold">
+        some thing went wrong
+        <h3 className="text-gray-500 ">Error Message : {error.message}</h3>
+      </div>
+      <button
+        onClick={reset}
+        className="bg-blue-700 hover:bg-blue-800 text-white px-5 py-2 rounded-lg mt-5"
+        >
+        Try Again
+        </button>
+      <Link className="text-xl underline text-blue-700 block mt-6" href="/">
+        Go back to home page
+      </Link>
+    </div>
+  );
+}
+
+export default AriclesErrorPage;
