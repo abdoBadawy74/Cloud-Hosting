92fda050a1a486414a49e9ef448924971ac1e092
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-07 08:12:01 +0200

    plans section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
-import Hero from "@/Components/home/hero";
+import Hero from "@/Components/home/Hero";
+import WebHostingPlan from "@/Components/home/WebHostingPlan";
 
 function HomePage() {
   console.log("this is home ");
   return (
     <>
-      <Hero/>
+      <Hero />
+      <h2 className="text-center mt-10 text-3xl font-bold">
+        Choose Your Web Hosing Plan
+      </h2>
+      <div className="container m-auto flex justify-center items-center my-7 flex-wrap md:gap-7">
+        <WebHostingPlan />
+        <WebHostingPlan />
+        <WebHostingPlan />
+      </div>
     </>
   );
 }
