70915af3f05d8a4eba74724ee87105ca8604c2a3
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-07 05:05:08 +0200

    edit error in routing | add image

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -6,8 +6,8 @@ interface SingleArticlePageProps {
   };
 }
 
-const SingleArticlePage = async ({ params }: SingleArticlePageProps) => {
-  const { id } = params;
+const SingleArticlePage = async (props: SingleArticlePageProps) => {
+  const { id } = await props.params;
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
   if (!res.ok) {
     throw new Error("Failed to fetch article from the server");
@@ -17,7 +17,9 @@ const SingleArticlePage = async ({ params }: SingleArticlePageProps) => {
   return (
     <section className="fixed-height container m-auto w-full px-5 pt-8 md:w-3/4">
       <div className="bg-white p-7 rounded-lg">
-        <h1 className="text-3xl font-bold text-gray-700 mb-2">{article.title}</h1>
+        <h1 className="text-3xl font-bold text-gray-700 mb-2">
+          {article.title}
+        </h1>
         <div className="text-gray-500">1/7/2024</div>
         <p className="text-gray-800 text-xl mt-5">{article.body}</p>
       </div>
