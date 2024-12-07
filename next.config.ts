3dc5691d2360c97406126274a95cf92ec559873e
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-07 07:50:57 +0200

    turbo mode

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    turbo: true,
+    turbo: {
+      rules: {
+        "*.mdx": ["mdx-loader"], // استخدم النمط الجديد للملفات
+      },
+    },
   },
-  
 };
 
 export default nextConfig;
