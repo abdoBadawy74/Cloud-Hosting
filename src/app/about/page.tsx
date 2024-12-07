8d76c6467c24f3dd5f0d14c67e0df39085cea6fc
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-07 08:16:55 +0200

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function AboutPage() {
   return (
-    <div>AboutPage</div>
-  )
+    <div className="fixed-height container m-auto">
+      <h1 className="text-3xl font-bold text-gray-800 p-5">About this app</h1>
+      <p className="px-5 text-gray-600 text-xl">
+        This is a simple app that demonstrates how to use React Router with
+        TypeScript. It includes a home page, an about page, and a 404 page.
+      </p>
+    </div>
+  );
 }
 
-export default AboutPage
+export default AboutPage;
