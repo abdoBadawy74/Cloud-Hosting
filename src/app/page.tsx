9739a13cbbb4b8acec91af9132e12e325c873a16
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-07 07:19:35 +0200

    hero section design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/Components/home/hero";
+
 function HomePage() {
   console.log("this is home ");
   return (
     <>
-      <div>Home Page</div>
-      <p> Welcome to Next js </p>
+      <Hero/>
     </>
   );
 }
