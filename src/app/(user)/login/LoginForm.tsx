31adb98631ea65a02ef54771918fd5da4b31ac45
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-09 08:51:31 +0200

    move form to seperate copm and make it client side

---
+++ src/app/(user)/login/LoginForm.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { useState } from "react";
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e:React.FormEvent) => {
+    e.preventDefault();
+    console.log(email, password);
+    }
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col">
+      <input
+        className="mb-4 border rounded p-2 text-xl"
+        type="email"
+        placeholder="Enter your email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        className="mb-4 border rounded p-2 text-xl"
+        type="password"
+        placeholder="Enter your password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="text-2xl text-white bg-blue-800 rounded p-2 w-full"
+      >
+        Login
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
--- src/app/(user)/login/page.tsx
+++ src/app/(user)/login/page.tsx
@@ -1,28 +1,11 @@
-"Use Client";
+import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
   return (
     <section className="fixed-height container m-auto px-7 flex items-center justify-center">
       <div className="m-auto bg-white rounded-lg p-5 w-full md:2/3">
         <h1 className="text-3xl font-bold text-gray-800 mb-5">Login</h1>
-        <form className="flex flex-col">
-          <input
-            className="mb-4 border rounded p-2 text-xl"
-            type="email"
-            placeholder="Enter your email"
-          />
-          <input
-            className="mb-4 border rounded p-2 text-xl"
-            type="password"
-            placeholder="Enter your password"
-          />
-          <button
-            type="submit"
-            className="text-2xl text-white bg-blue-800 rounded p-2 w-full"
-          >
-            Login
-          </button>
-        </form>
+        <LoginForm/>
       </div>
     </section>
   );
