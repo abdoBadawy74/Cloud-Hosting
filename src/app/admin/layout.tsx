5d061fd14fd8eff25add8d9688cdc38fa9d72e25
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-12-11 02:55:22 +0200

    side bar design

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,11 +6,13 @@ interface AdminDashboardLayoutProps {
 
 const AdminDashboardLayout = ({ children }: AdminDashboardLayoutProps) => {
   return (
-    <div>
-      <div>
+    <div className="overflow-height flex items-start justify-between overflow-hidden">
+      <div className="overflow-height w-15 lg:w-1/5 bg-purple-600 text-white p-1 lg:p-5">
         <AdminSideBar />
       </div>
-      <div>{children}</div>
+      <div className="overflow-height w-full lg:w-4/5 overflow-y-scroll">
+        {children}
+      </div>
     </div>
   );
 };
